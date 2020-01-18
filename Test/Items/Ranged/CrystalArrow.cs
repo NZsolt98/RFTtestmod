96d95b62a50f6d3a9b4499deb41628760e29fea4
Alkaszta <43853063+Alkaszta@users.noreply.github.com>
2020-01-18 15:47:13 +0100

    Crystal Arrow
    Crystal Arrow hozzáadása, cs és png fájl.

---
+++ Test/Items/Ranged/CrystalArrow.cs
@@ -0,0 +1,28 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Test.Items.Ranged
+{
+	public class CrystalArrow : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("This is a modded arrow ammo.");
+		}
+
+		public override void SetDefaults() {
+			item.damage = 8;
+			item.ranged = true;
+			item.width = 14;
+			item.height = 32;
+			item.maxStack = 999;
+			item.consumable = true;             //You need to set the item consumable so that the ammo would automatically consumed
+			item.knockBack = 1.5f;
+			item.value = 10;
+			item.rare = 2;
+			item.shoot = mod.ProjectileType("CrystalArrow");   //The projectile shoot when your weapon using this ammo
+			item.shootSpeed = 15f;                  //The speed of the projectile
+			item.ammo = AmmoID.Arrow;              //The ammo class this ammo belongs to.
+		}
+	}
+}
